cbe9ffc01d0ccfc4aa2ab3599b61e6fa5dc71b77
Hirotaka Uchiyama <hirotaka.desu@icloud.com>
2022-12-29 04:49:55 +0900

    最初のコミット

--- myapp/Lesson10/index.php
+++ myapp/Lesson10/index.php
@@ -1,5 +1,7 @@
 <?php
 // jsonファイルを読み込んで id と各都道府県名（short）を表⽰させる
+//下記の形式の配列に値をセットして表⽰させる
+//[ "都道府県id" => [市町村名1, 市町村名2...], 
 $json = "./sample.json";
 $file = file_get_contents($json);
 $file = mb_convert_encoding($file, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
@@ -11,14 +13,19 @@ foreach($arr[0] as $key => $val){
     echo "id". (int)$val["id"]. "は". $val["short"]. "<br>";
 }
 
-//下記の形式の配列に値をセットして表⽰させる
-//[ "都道府県id" => [市町村名1, 市町村名2...], 
-foreach($arr[0] as $key => $val){
-    $arr = array();
-    foreach($val['city'] as $cities){
-        array_push($arr,$cities);
+$city_data = [];
+foreach ($arr as $key => $val) {
+    foreach($val as $key2 => $val2){
+    $name = $val2['name'];
+    $id = $val2['id']; 
     }
-    print_r($arr);
 }
 
-?>
+var_dump($city_data);
+
+
+$city1 = array_column($arr, 'key');
+foreach($city1 as $id){
+    print $id .PHP_EOL;
+}
+
